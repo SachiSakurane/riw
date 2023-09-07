c2b3b8a1e7131a507e469057654f302ab8d6d622
Sachi Sakurane <sakuranesachi@gmail.com>
2023-09-08 02:50:14 +0900

    add oneshot

---
+++ include/riw/utility/oneshot.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cmath>
+#include <chrono>
+
+#include <riw/easing.hpp>
+
+namespace riw
+{
+  template <std::floating_point ValueType = float>
+  class oneshot
+  {
+  public:
+    explicit oneshot(const std::chrono::milliseconds dur) : duration{dur}
+    {
+    }
+
+    const std::chrono::milliseconds duration;
+
+    bool is_end() const
+    {
+      return duration > (std::chrono::steady_clock::now() - update_time);
+    }
+
+    template <class Easing = riw::quint>
+    ValueType get_out() const
+    {
+      const auto d = std::chrono::steady_clock::now() - update_time;
+      const auto v = std::clamp(static_cast<ValueType>(std::chrono::duration_cast<std::chrono::milliseconds>(d).count()) / static_cast<float>(duration.count()), static_cast<ValueType>(0), static_cast<ValueType>(1));
+      return riw::ease_out<Easing>(v);
+    }
+
+    void reset() {
+      update_time = std::chrono::steady_clock::now();
+    }
+
+  private:
+    std::chrono::steady_clock::time_point update_time{std::chrono::steady_clock::now()};
+  };
+} // namespace riw
