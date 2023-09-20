933119988cf970b6500e02a1d6fd3e9e2e01f22f
Sachi Sakurane <sakuranesachi@gmail.com>
2023-09-21 01:52:06 +0900

    ranged

--- include/riw/value_range/ranged.hpp
+++ include/riw/value_range/ranged.hpp
@@ -8,7 +8,7 @@
 namespace riw {
 template <riw::arithmetic Type, riw::value_range<Type> Range>
 struct ranged {
-  static constexpr decltype(auto) range = Range;
+  static constexpr decltype(auto) range_value = Range;
   constexpr ranged(Type v) : value{riw::clamp(v, Range)} {}
 
   ranged(const ranged &) = default;
