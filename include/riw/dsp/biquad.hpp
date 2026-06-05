79add5e45de714fa4611cd2e8f392468a840d4e4
Sachi Sakurane <sakuranesachi@gmail.com>
2026-06-05 17:42:29 +0900

    Add stateful biquad filter

--- include/riw/dsp/biquad.hpp
+++ include/riw/dsp/biquad.hpp
@@ -8,6 +8,42 @@
 
 namespace riw {
 template <std::floating_point FloatType>
+class biquad_filter {
+public:
+  using coefficients_type = std::array<FloatType, 6>;
+
+  constexpr void set_coefficients(const coefficients_type &coefficients) {
+    const auto inv_a0 = static_cast<FloatType>(1) / coefficients[3];
+    b0 = coefficients[0] * inv_a0;
+    b1 = coefficients[1] * inv_a0;
+    b2 = coefficients[2] * inv_a0;
+    a1 = coefficients[4] * inv_a0;
+    a2 = coefficients[5] * inv_a0;
+  }
+
+  constexpr void reset() {
+    s1 = static_cast<FloatType>(0);
+    s2 = static_cast<FloatType>(0);
+  }
+
+  constexpr FloatType filter(FloatType in) {
+    const auto out = s1 + b0 * in;
+    s1 = s2 + b1 * in - a1 * out;
+    s2 = b2 * in - a2 * out;
+    return out;
+  }
+
+private:
+  FloatType b0 = static_cast<FloatType>(1);
+  FloatType b1 = static_cast<FloatType>(0);
+  FloatType b2 = static_cast<FloatType>(0);
+  FloatType a1 = static_cast<FloatType>(0);
+  FloatType a2 = static_cast<FloatType>(0);
+  FloatType s1 = static_cast<FloatType>(0);
+  FloatType s2 = static_cast<FloatType>(0);
+};
+
+template <std::floating_point FloatType>
 constexpr auto make_highpass_coefficients(FloatType sample_rate, FloatType cutoff_freq,
                                           FloatType resonance) {
   const auto omega = riw::two_pi<FloatType> * cutoff_freq / sample_rate;
